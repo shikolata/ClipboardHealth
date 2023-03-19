8efc70219576f1b3fdc516d0ad1012c2bf72f38f
ShiKoLatA <sabouela@lakeheadu.ca>
2023-03-19 19:00:49 +0200

    Added index.js

---
+++ index.js
@@ -0,0 +1,4 @@
+const {deterministicPartitionKey} = require("./dpk");
+
+console.log(`This was a fun exercise!`);
+console.log(`deterministicPartitionKey's trivial output is: ${deterministicPartitionKey()}`);
